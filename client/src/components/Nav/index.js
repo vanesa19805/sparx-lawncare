35a3c99c42e831342a405f3914f923c84dd4fc28
Gopal Patel <patelgopalk1@gmail.com>
2021-05-19 14:41:21 -0400

    Added Nav bar with links

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavLinks(props) {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link> 
+          </Nav>          
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavLinks;
