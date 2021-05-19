023eb490b09526f2dce1167263b7f54b3ec7d0ef
Gopal Patel <patelgopalk1@gmail.com>
2021-05-19 14:40:48 -0400

    Added Header component for app title

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+function Header(props) {
+    return (
+        <div className="heading">
+            <h1>Sparx Lawncare</h1>
+            <p>Lawn care, landscaping, and snow removal made simple</p>
+        </div>
+    );
+}
+
+export default Header;
