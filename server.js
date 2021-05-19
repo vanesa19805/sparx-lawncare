eb326a7c4a511ed9201b82f78354e733c7fa8ce6
Kajal Patel <kajalpatel20@gmail.com>
2021-05-19 18:44:15 -0400

    loginpage

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express  = require('express');
 const app      = express();
 const port     = process.env.PORT || 3001;
-const session = require('express-session');
-const passport = require('passport');
-const path= require("path") 
+// const session = require('express-session');
+// const passport = require('passport');
+// const path= require("path") 
 
 
 
@@ -14,27 +14,27 @@ app.use(express.json())
 app.use(require("./routes"))
 
 // Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-  }));
+// app.use(session({
+//     secret: 'secret',
+//     saveUninitialized: true,
+//     resave: true
+//   }));
   
-  // Passport init
-  app.use(passport.initialize());
-  app.use(passport.session());
+//   // Passport init
+//   app.use(passport.initialize());
+//   app.use(passport.session());
 
   if(process.env.NODE_ENV==="production"){
-      app.use(express.static("./client/build"))
+      app.use(express.static("client/build"))
        //routes
 
-       app.get("*", function (req,res){
-           res.sendFile(path.join(__dirname,"./client/build/index.html"))
-       })
+      //  app.get("*", function (req,res){
+      //      res.sendFile(path.join(__dirname,"./client/public/index.html"))
+      //  })
 
   }
 // this allowes you to activate the port
-  require("./config//connection")
+  // require("./config//connection")
   app.listen(port,function(){
       console.log("app is listening on port", port)
   } )
