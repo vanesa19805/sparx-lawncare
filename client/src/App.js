142123618456eb24a0e25521cfd0c7f2b0dee241
Kajal Patel <kajalpatel20@gmail.com>
2021-05-22 12:55:22 -0400

    create signupform, loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import "./App.css";
 import Header from './components/Header';
 import NavLinks from './components/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NavLinks />
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    return (
+
+      <Router>
+        <div className="App">
+          <Header />
+          <NavLinks />
+        </div>
+        <div>
+          <Switch>
+            <Route exact path="/" component={LoginForm} />
+            <Route exact path="/search" component={SignupForm} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -17,6 +17,7 @@ function NavLinks(props) {
         </Navbar.Collapse>
       </Navbar>
     </div>
+    
   );
 }
 
