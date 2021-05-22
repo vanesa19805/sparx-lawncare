e12cc0f837b6fc7ddb246952d98d23c01e28596e
Gopal Patel <patelgopalk1@gmail.com>
2021-05-22 13:35:58 -0400

    Added bout

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import NavLinks from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavLinks />
-      
+      <About />
     </div>
   );
 }
--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,8 +6,7 @@ function About(props) {
     <div>
       <Card>
         <Card.Header>About Us</Card.Header>
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
+        <Card.Body>          
           <Card.Text>
             Sparx lawncare service is here to provide you with expert assistance. We specialize in lawn maintence, tree removal, and snow removal.
             Schedule your service online or give us a call more info. 
