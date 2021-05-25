6bae373559900a2f2fa4698a20f094cc559b7fd3
Gopal Patel <patelgopalk1@gmail.com>
2021-05-24 21:17:06 -0400

    Added footer, fixed header text

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+function Footer(props) {
+    return (
+        <div>
+           <footer>
+               <h5 className="text-center">Copyright @2021 Sparx Lawncare - All Rights Reserved.</h5>
+            </footer> 
+        </div>
+    );
+}
+
+export default Footer;
