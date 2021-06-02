e4a4b4f2cc77ed9eb301f0681286c4844e02fe56
Gopal Patel <patelgopalk1@gmail.com>
2021-06-02 19:22:23 -0400

    Added card around login

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Api from "../../utils/API";
+import Card from "react-bootstrap/Card";
 
 function LoginForm() {
   const [username, setUserName] = useState("");
@@ -14,9 +15,9 @@ function LoginForm() {
       username: username,
       password: password,
     };
-    console.log(obj)
+    console.log(obj);
     Api.loginUser(obj).then((res) => {
-        console.log(res);
+      console.log(res);
       history.push("/user-home");
     });
   };
@@ -31,48 +32,56 @@ function LoginForm() {
   };
 
   return (
-    <>
-      <form onSubmit={handleFormSubmit} className="m-3">
-        <h2>LOGIN</h2>
-        <section className="mb-3">
-          <label for="username" className="form-label">
-            User Name:
-          </label>
-          <br></br>
-          <input
-            placeholder="Username"
-            name="username"
-            type="username"
-            id="username"
-            onChange={handleChange}
-          />
-        </section>
-        <section className="mb-3">
-          <label for="user-password" className="form-label">
-            Password
-          </label>
-          <br></br>
-          <input
-            placeholder="password"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </section>
-        <button id="login" className="btn btn-dark localBtn">
-          Login
-        </button>
-        <section className="m-3 justify-content-end text-end">
-          <label for="signupInstead" className="form-label">
-            Not A Member Yet! Please Signup!
-          </label>
-          <Link id="signupInstead" to="/signup" class="btn btn-light localBtn">
-            Signup
-          </Link>
-        </section>
-      </form>
-    </>
+    <div className="col-6 mx-auto p-5">
+      <Card>
+        <Card.Header><h3 className="text-center">LOGIN</h3></Card.Header>
+        <Card.Body>
+          <form onSubmit={handleFormSubmit} className="m-3">
+            <section className="mb-3">
+              <label for="username" className="form-label">
+                User Name:
+              </label>
+              <br></br>
+              <input
+                placeholder="Username"
+                name="username"
+                type="username"
+                id="username"
+                onChange={handleChange}
+              />
+            </section>
+            <section className="mb-3">
+              <label for="user-password" className="form-label">
+                Password
+              </label>
+              <br></br>
+              <input
+                placeholder="password"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </section>
+            <button id="login" className="btn btn-primary localBtn">
+              Login
+            </button>
+            <section className="m-3 justify-content-end text-end">
+              <label for="signupInstead" className="form-label pe-2">
+                Not A Member Yet! Please Signup!
+              </label>
+              <Link
+                id="signupInstead"
+                to="/signup"
+                class="btn btn-primary localBtn"
+              >
+                Signup
+              </Link>
+            </section>
+          </form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
