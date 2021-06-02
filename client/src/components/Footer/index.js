508cc577d4f627a6bcc2923e550ef306325badc4
Zakk Haug <zakklikeslamps@gmail.com>
2021-06-02 19:18:56 -0400

    Footer good.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,6 +4,7 @@ import "./style.css";
 function Footer(props) {
     return (
         <div>
+            
            <footer>
                <h5 className="text-center">Copyright @2021 Sparx Lawncare - All Rights Reserved.</h5>
             </footer> 
