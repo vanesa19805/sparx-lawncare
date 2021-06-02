1327abc111902be90941e7ca2d492f626aba0592
Gopal Patel <patelgopalk1@gmail.com>
2021-06-02 19:43:59 -0400

    Fixed issue in login

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -33,57 +33,6 @@ function LoginForm() {
 
   return (
 
-    <>
-      <form onSubmit={handleFormSubmit} className="m-3">
-        <h2>LOGIN</h2>
-        <section className="mb-3">
-          <label for="username" className="form-label">
-            User Name:
-          </label>
-          <br></br>
-          <input
-            placeholder="Username"
-            name="username"
-            type="username"
-            id="username"
-            onChange={handleChange}
-          />
-        </section>
-        <section className="mb-3">
-          <label for="user-password" className="form-label">
-            Password:
-          </label>
-          <br></br>
-          <input
-            placeholder="password"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </section>
-        <button id="login" className="btn btn-dark localBtn">
-          Login
-        </button>
-        <section className="m-3 justify-content-end text-end">
-          <label for="signupInstead" className="form-label">
-            Not A Member Yet! Please Signup!
-          </label>
-          <Link id="signupInstead" to="/signup" class="btn btn-light localBtn">
-            Signup
-          </Link>
-        </section>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-      </form>
-    </>
-
     <div className="col-6 mx-auto p-5">
       <Card>
         <Card.Header><h3 className="text-center">LOGIN</h3></Card.Header>
