cbae1af30e5c383153882452413c122d78aa7e6f
vanesa19805 <gvane1919@gmail.com>
2021-06-07 13:24:32 -0400

    deploy test

--- routes/api/apt.js
+++ routes/api/apt.js
@@ -1,7 +1,7 @@
 const router = require ("express").Router();
 const mongoose =require("mongoose");
 const path =require("path");
-const { brotliDecompress } = require("zlib");
+ 
 
 
 const db = require("../../models");
@@ -40,45 +40,4 @@ router.post("/aptform", ({ body ,user}, res) => {
 
 
   
-module.exports = router;const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const EventSchema = new Schema({
-  location: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  contact: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  service: {
-    type: String,
-    trim: true
-  },
-  time:{
-    type: String,
-    required: true
-  },
-  day:{
-    type: Date,
-    default: new Date().setDate(new Date().getDate())
-  },
-  note:{
-      type: String,
-      trim: true
-  },
-  userId:{
-    type : Schema.Types.ObjectId,
-    ref: "User"
-  }
-
-});
-
-const Event = mongoose.model("event", EventSchema);
-
-module.exports = Event;
-
+module.exports = router 
