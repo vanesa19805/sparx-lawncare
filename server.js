ec4d67f091750cc3afefac6425aa99cff222e939
vanesa19805 <gvane1919@gmail.com>
2021-06-07 13:30:19 -0400

    correcting path

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 mongoose.connect(
